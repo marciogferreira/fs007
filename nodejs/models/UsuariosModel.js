430f6d1e6ea017d44e15f521847beb05c1038339
Márcio Ferreira <hmarciogferreira@gmail.com>
2024-07-06 17:01:46 -0300

    Token

--- nodejs/models/UsuariosModel.js
+++ nodejs/models/UsuariosModel.js
@@ -1,5 +1,7 @@
 import Conexao from "../config/conexao.js";
+import Helpers from "../helpers/Helpers.js";
 import BaseModel from "./BaseModel.js";
+
 class UsuariosModel extends BaseModel {
 
     constructor() {
@@ -9,9 +11,19 @@ class UsuariosModel extends BaseModel {
     async login(email, senha) {
 
         const conexao = new Conexao();
+        senha = Helpers.getHash(senha);
+        console.log("Senha", senha);
         const sql = `SELECT * FROM ${this.tableName} WHERE login =? AND senha =?`;
         const dados = await conexao.query(sql, [email, senha]);
         return dados[0];
+    }
+
+    async create(body) {
+        const conexao = new Conexao();
+        body.senha = Helpers.getHash(body.senha);
+        const sql = `INSERT INTO ${this.tableName} SET ?`;
+        const dados = await conexao.query(sql, body)
+        return dados;
     }
 
 }
